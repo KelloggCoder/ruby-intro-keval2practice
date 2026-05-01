b3dc9125d2bd60976eda4d7201e1e3c51572fd0c
KelloggCoder <keval.sheth@kellogg.northwestern.edu>
2026-05-01 02:27:04 +0000

    completion of lab1

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,40 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
-
+puts 2
+puts 5
 # Non-numbers
-
+puts 2 + 5
+puts 2 - 5
+puts 2 + 5 * 5
 # True, False
 
 # Nothing
 
 # Variables
-
+x = 2
+y = 5
+puts x
+puts y
+Tacos = "tacos:"
+Quantity = 3.to_s
+puts Tacos + Quantity 
 # Combine strings and variables
+first_name = "Keval"
+greeting = "Hello, #{first_name} !"
+puts greeting
 
 # String manipulation
+puts "Hello, WOrld"
+puts "ruby is"  + " fun"
+puts "tacos" * 3
+puts "tacos" + 3.to_s
+
+puts "Hello      " + "there!"
+puts "Hello".length
+puts "how are you today?".upcase
+puts "computer".reverse.upcase
+creed = "This Is The Way"
+puts creed.upcase
+puts creed.downcase
+puts creed.swapcase
--- labs/1-data.rb
+++ labs/1-data.rb
@@ -10,6 +10,15 @@
 # then write them out to the screen. Show the total
 # as well.
 
+roll1 = rand(1..6)
+roll2 = rand (1..6)
+total = roll1 + roll2
+
+puts " Roll 1 = #{roll1}"
+puts " Roll 2 = #{roll2}"
+puts "Total = #{total} !"
+
+
 # HINTS
 # Get a random number between 1 and 6 by doing:
 # rand(1..6)
