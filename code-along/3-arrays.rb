69ace8d8cb8ffbc9460b8617c3a0bca192aeb89f
KelloggCoder <keval.sheth@kellogg.northwestern.edu>
2026-05-01 03:38:54 +0000

    lab 3 completed

--- code-along/3-arrays.rb
+++ code-along/3-arrays.rb
@@ -3,10 +3,34 @@
 # ruby 3-arrays.rb
 
 # Arrays are lists of things
+favorite_foods = ["pizza", 'apples', 'ice cream']
+puts favorite_foods
 
+numbers = [1, 2, 5]
+
+we_have_to_go_back = [4, 8, 15, 16, 23, 42]
+puts we_have_to_go_back
+
+mixed_array = ["tacos", 12, true]
+puts mixed_array
+
+shopping_lists = [["milk", "eggs", "toilet paper"], ["soap", "shampoo", "shaving cream", "toilet paper"]]
+puts shopping_lists
 # Accessing the array
 
-# Add to the array
+first_fav_food = favorite_foods[4]
+puts first_fav_food
 
+puts shopping_lists [1][2]
+# Add to the array
+favorite_foods.push("pickles")
+puts favorite_foods
+
+favorite_foods = favorite_foods + ['fries']
+puts favorite_foods
+puts favorite_foods.length
+puts favorite_foods.count
+puts favorite_foods.size
+puts numbers.sum 
 # There are lots of fun things you can do with arrays:
 # https://ruby-doc.org/core-2.7.0/Array.html
--- labs/3-arrays.rb
+++ labs/3-arrays.rb
@@ -12,6 +12,12 @@
 # If the two lists contain the same item, only show it once!
 # Lastly, display each item in the list prepended with "buy ".
 
+my_list = ['milk', 'eggs', 'bacon', 'apples']
+friend_list = ['beer', 'cookies', 'apples']
+
+complete_list = my_list + friend_list
+puts complete_list.sort.uniq.each{ |s| s.prepend "Buy:"}
+
 # HINTS
 # Learn to read the documentation!
 # https://ruby-doc.org/core-2.7.0/Array.html
